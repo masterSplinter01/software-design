f8bf39f65a59f915f77ea3cb8bf70b35986c4ebf
dan <dan@debian.treacy>
2018-03-02 02:14:57 +0300

    lexer correction

--- cli/src/Lexer.py
+++ cli/src/Lexer.py
@@ -1,11 +1,13 @@
 import src.ply.lex as lex
+import src.ply.yacc as yacc
 
 tokens = ('ASSIGNMENT', 'WORD', 'PIPE', 'QUOTED_STRING',)
 
-t_PIPE = r'\|'
-t_QUOTED_STRING = r'(\"|\')(?:[^\\]|(?:\\.))*(\"|\')'
+
 t_ASSIGNMENT = r'(?<!\s)=(?!\s)'
 t_WORD = r'[a-zA-Z0-9_$][a-zA-Z0-9_$]*|(?<=\s)=(?=\s)'
+t_PIPE = r'\|'
+t_QUOTED_STRING = r'(\"|\')(?:[^\\]|(?:\\.))*(\"|\')'
 t_ignore = ' \n\t'
 
 
@@ -14,31 +16,14 @@ def t_error(t):
     t.lexer.skip(1)
 
 
-class Lexer:
-    def __init__(self):
-        self.lexer = lex.lex()
-
-    def get_tokens(self, input_str):
-        self.lexer.input(input_str)
-        result_tokens = []
-        while True:
-            tok = self.lexer.token()
-            if not tok:
-                break
-            result_tokens.append(tok)
-        return result_tokens
-
-import src.ply.yacc as yacc
-
-
 def p_assignment(p):
     '''assignment : WORD ASSIGNMENT WORD'''
+
 
 def p_command(p):
     '''command : WORD
                 | command WORD
                 | command QUOTED_STRING '''
-
 
 
 def p_command_line(p):
@@ -47,16 +32,7 @@ def p_command_line(p):
 
 
 data = 'echo "sdf" | x=13| x = 13'
-lx = Lexer()
-tokend = lx.get_tokens(data)
+lexer = lex.lex()
+lexer.input(data)
 parser = yacc.yacc()
 print(parser.parse(data))
-
-
-'''
-data = 'echo "sdf" | x=13 | x = 13'
-lx = Lexer()
-tok = lx.get_tokens(data)
-print(tok)
-
-'''
